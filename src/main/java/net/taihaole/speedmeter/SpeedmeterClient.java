afa3ee85b3ff834217fd8f9350705f59bb946e76
Your Name <you@example.com>
2025-07-19 15:20:11 -0400

    Initial commit

---
+++ src/main/java/net/taihaole/speedmeter/SpeedmeterClient.java
@@ -0,0 +1,10 @@
+package net.taihaole.speedmeter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpeedmeterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
